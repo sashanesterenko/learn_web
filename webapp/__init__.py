e9be39b7897ba85b6522c908e101100d4074ebea
Sasha Nesterenko <nesterenko.sasha@gmail.com>
2019-03-23 22:32:40 +0200

    Add database support by Flask-SQLAchemy

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template
 
+from webapp.model import db, News
 from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
 
 def create_app():
 
-	app = Flask(__name__)
-	app.config.from_pyfile('config.py')
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
 
-	@app.route('/')
-	def index():
-	    title = "LP News"
-	    weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
-	    news_list = get_python_news()
-	    # weather = False
-	    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+    @app.route('/')
+    def index():
+        title = "LP News"
+        weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
+        news_list = News.query.order_by(News.published.desc()).all()
+        # print(news_list)
+        # weather = False
+        return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
 
-	return app
+    return app
